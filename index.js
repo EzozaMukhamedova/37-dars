7645b0d7c1b1d4f8af9c09f2a5414d0f1f9ba62c
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2024-12-05 00:30:33 +0900

    37-dars

---
+++ index.js
@@ -0,0 +1,192 @@
+// Begin1
+// let a = +prompt("A ni kiriting");
+// let p = 4 * a;
+
+// alert("p = " + p);
+
+// Begin2
+// let a = +prompt("A ni kiriting");
+// let s = Math.pow(a, 2);
+
+// alert("s = " + s);
+
+// Begin3
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let s = a * b;
+// let p = 2 * (a + b);
+
+// alert("s = " + s);
+// alert("p = " + p);
+
+// Begin4
+// let d = +prompt("D ni kiriting");
+// let L = Math.PI * d;
+
+// alert("L = " + L);
+
+// Begin5
+// let a = +prompt("A ni kiriting");
+// let V = Math.pow(a, 3);
+// let S = 6 * Math.pow(a, 2);
+
+// alert("V = " + V);
+// alert("S = " + S);
+
+// Begin6
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let c = +prompt("C ni kiriting");
+
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+
+// alert("V = " + V);
+// alert("S = " + S);
+
+// Begin7
+// let R = +prompt("R ni kiriting");
+
+// let L = 2 * Math.PI * R
+// let S = Math.PI * R ** 2
+
+// alert("L= " + L);
+// alert("S= " + S);
+
+// Begin8
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+
+// let c = (a+b)/2;
+
+// alert("c=" + c);
+
+// Begin 13
+// let R1 = +prompt("R1 ni kriting");
+// let R2 = +prompt("R2 ni kriting");
+
+// let S1 = Math.PI * R1;
+// let S2 = Math.PI * R2;
+// let S3 = Math.PI * (R1 - R2);
+
+// alert("S1= " + S1);
+// alert("S2= " + S2);
+// alert("S3= " + S3);
+
+// Begin 32
+// let Tf = +prompt("Tf ni kiriting");
+
+// let Tc = (Tf - 32) * 5/9;
+
+// alert("Tc= " + Tc);
+
+// Ikkinchi vazifa
+
+// Integer1
+// let L = +prompt("L sonini kiriting");
+
+// let m = L/100;
+
+// alert("m= " + m);
+
+// Integer2
+// let m = +prompt("m kg ni kiriting");
+
+// let t = m / 1000;
+
+// alert("t= " + t);
+
+// Integer3
+// let b = +prompt("baytni kiriting");
+
+// let kb = b / 1024;
+
+// alert("kb= " + kb);
+
+// integer4
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+
+// let j = a / b;
+
+// alert("j= " + j);
+
+// Integer5
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+
+// let j = a % b;
+
+// alert("ortib qolgan son = " + j);
+
+// integer6
+// let a = +prompt("Ikki xonali sonini kiriting");
+
+// let on = Math.floor(a / 10);
+// let bir = a % 10;
+
+// alert("onliklar xonasi = " + on);
+// alert("birliklar xonasi = " + bir);
+
+// integer7
+// let a = +prompt("Ikki xonali sonini kiriting");
+
+// let on = Math.floor(a / 10);
+// let bir = a % 10;
+// let y = on + bir;
+// alert("Yegindisi = " + y);
+
+// integer8
+// let a = +prompt("Ikki xonali sonini kiriting");
+
+// let on = Math.floor(a / 10);
+// let bir = a % 10;
+// let al = bir * 10 + on;
+
+// alert("Almashtirilgan holat = " + al);
+
+// integer9
+// let a = +prompt("Uch xonali sonini kiriting");
+
+// let yuz = Math.floor(a / 100);
+
+// alert("Yuzlar xonasidagi raqam = " + yuz);
+
+// integer10
+// let a = +prompt("Uch xonali sonini kiriting");
+
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+
+// alert("Birliklar xonasi = " + bir);
+// alert("O'nliklar xonasi = " + on);
+
+// integer11
+// let a = +prompt("Uch xonali sonini kiriting");
+
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// let yuz = Math.floor(a / 100);
+// let yeg = bir + on + yuz;
+
+// alert("Raqamlar yegindisi = " + yeg);
+
+// integer12
+// let a = +prompt("Uch xonali sonini kiriting");
+
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// let yuz = Math.floor(a / 100);
+// let yeg = (bir * 100) + (on * 10) + yuz;
+
+// alert("Teskari tartibda = " + yeg);
+
+// integer13
+// let a = +prompt("Uch xonali sonini kiriting");
+
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// let yuz = Math.floor(a / 100);
+// let yeg = (on * 100) + (bir * 10) + yuz;
+
+// alert("Teskari tartibda = " + yeg);
